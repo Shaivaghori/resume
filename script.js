ec904aeaa0a9d3824899eac38d5109335c0a1a02
Shaivaghori <90083816+Shaivaghori@users.noreply.github.com>
2022-03-14 16:17:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var btnup=document.getElementById("btn5");
+btnup.addEventListener("click",goup);
+
+function goup(){
+    window.scrollTo(0,0);
+}
+
+// var cvbtn=document.getElementById("btn2");
+// cvbtn.addEventListener("click",downloadcv);
+
+// function downloadcv(){
+    
+// }
